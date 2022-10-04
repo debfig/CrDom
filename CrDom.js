589680a3acc09704007b51cd95feb8115b911507
灿烈 <3233834670@qq.com>
2022-10-04 19:21:19 +0800

    重构

--- CrDom.js
+++ CrDom.js
@@ -1,56 +1,100 @@
 (function (window, undefined) {
 
-    class CrDom {
-        constructor() {
-            this.eq = null;
-        }
 
-        //获取DOM元素 存放在 this.eq 中
-        _$(dom) {
-            this.eq = typeof dom === "string" ? document.querySelectorAll(dom) : dom;
-            return this
-        }
+    //定义$方法,返回一个对象
+    function $(dom) {
 
-        //修改样式
-        _$style(style, value) {
-            if (typeof style === "object") {
-                for (let i in style) {
-                    for (let k of this.eq) {
-                        k.style[i] = style[i];
-                    }
-                }
-            } else if (typeof style === "string" && typeof value === "string") {
-                for (let k of this.eq) {
-                    k.style[style] = value;
-                }
-            }
-            return this
-        }
-
-        //绑定事件
-        _$on(event, Callback) {
-            for (let i of this.eq) {
-                i.addEventListener(event, Callback);
-            }
-            return this
-        }
-
-        //设置 this 指向
-        _$this(thiss) {
-            this.eq = [thiss];
-            return this
-        }
-
-        //修改元素内容
-        _$text(txt) {
-            for (let i of this.eq) {
-                i.innerText = txt;
-            }
-            return this
-        }
-
-
+        // 调用 $ 函数会返回一个 CrDom 实例
+        return new CrDom(dom);
     }
 
-    window.$ = new CrDom();
+    //* CrDom 构造函数
+    function CrDom(doms = '') {
+        this.dom = this.ifthis(doms);
+    }
+
+    //========================================================================================
+
+    //! 判断 $() 传入的是不是 this 
+    CrDom.prototype.ifthis = function (dom) {
+        if (typeof dom === 'object') {
+            return [dom];
+        } else if (typeof dom === 'string') {
+            return [...document.querySelectorAll(dom)];
+        }
+    }
+    //========================================================================================
+
+
+    //========================================================================================
+    //=========                  添加原型函数                  ================================
+    //========================================================================================
+
+    //TODO 选择 CrDom 中的dom 元素
+    CrDom.prototype.eq = function (value) {
+        this.dom = this.dom.filter((item, index, self) => {
+            return typeof value === 'number' ? index == value : self;
+        })
+        return this;
+    }
+
+    //TODO 修改样式
+    CrDom.prototype.css = function (style, value) {
+        if (typeof style === "object") {
+            for (let i in style) {
+                for (let k of this.dom) {
+                    k.style[i] = style[i];
+                }
+            }
+        } else if (typeof style === "string" && typeof value === "string") {
+            for (let k of this.dom) {
+                k.style[style] = value;
+            }
+        }
+        return this;
+    }
+
+    //TODO 绑定事件
+    CrDom.prototype.on = function (event, Callback) {
+        for (let i of this.dom) {
+            i.addEventListener(event, Callback);
+        }
+        return this;
+    }
+
+    //TODO dom中添加文本
+    CrDom.prototype.setText = function (txt) {
+        for (let i of this.dom) {
+            i.innerText = txt;
+        }
+        return this
+    }
+
+    //TODO 修改类名
+    CrDom.prototype.setClass = function (clas) {
+        if (typeof clas === 'string') {
+            for (let i of this.dom) {
+                i.className = clas
+            }
+        }
+        return this
+    }
+
+    //TODO 添加类名
+    CrDom.prototype.addClass = function (clas) {
+        if (typeof clas === 'string') {
+            for (let i of this.dom) {
+                i.classList.add(clas)
+            }
+        }
+        return this
+    }
+
+
+
+
+
+    //* 在 window 上添加 构造函数
+    window.$ = window.CrDom = $;
+
 })(window, undefined)
--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-## 项目 CrDom.js
+#  CrDom.js
+## 这是一个类似于jquery的js库，操作dom用来元素构造动画和一些其他的js方法
+
+1. $()方法如同jquery的选择器样，传入参数经过处理后获得dom元素，作为属性放在构造函数身上，之后所有的dom操作都是操作这个属性
+```
+   //定义$方法,返回一个对象
+    function $(dom) {
+
+        // 调用 $ 函数会返回一个 CrDom 实例
+        return new CrDom(dom);
+    }
+
+    //* CrDom 构造函数
+    function CrDom(doms = '') {
+        this.dom = this.ifthis(doms);
+    }
+
+    //========================================================================================
+
+    //! 判断 $() 传入的是不是 this 
+    CrDom.prototype.ifthis = function (dom) {
+        if (typeof dom === 'object') {
+            return [dom];
+        } else if (typeof dom === 'string') {
+            return [...document.querySelectorAll(dom)];
+        }
+    }
+```
+2. 在CrDom.js中可以同时获得多个dom元素，形成数组放在构造函数上，可用通过 eq()方法来选择要操作的dom
+```
+//TODO 选择 CrDom 中的dom 元素
+    CrDom.prototype.eq = function (value) {
+        this.dom = this.dom.filter((item, index, self) => {
+            return typeof value === 'number' ? index == value : self;
+        })
+        return this;
+    }
+```
+3. 修改样式 使用 css() 来更改通过传入对象或具体的值来修改
+```
+ //TODO 修改样式
+    CrDom.prototype.css = function (style, value) {
+        if (typeof style === "object") {
+            for (let i in style) {
+                for (let k of this.dom) {
+                    k.style[i] = style[i];
+                }
+            }
+        } else if (typeof style === "string" && typeof value === "string") {
+            for (let k of this.dom) {
+                k.style[style] = value;
+            }
+        }
+        return this;
+    }
+```
+```
+1. $(this).css('fontSize', '30px');
+2. $(this).css({
+       fontSize: '30px'
+    });
+```
+4. 绑定事件 通过 on() 来绑定事件
+```
+    //TODO 绑定事件
+    CrDom.prototype.on = function (event, Callback) {
+        for (let i of this.dom) {
+            i.addEventListener(event, Callback);
+        }
+        return this;
+    }
+```
+```
+ $('span').on('click', function () {
+        $(this).css('fontSize', '30px')
+    }).setText('hello')
+```
+5. 添加文本 添加类名 修改类名
+```
+    //TODO dom中添加文本
+    CrDom.prototype.setText = function (txt) {
+        for (let i of this.dom) {
+            i.innerText = txt;
+        }
+        return this
+    }
+
+    //TODO 修改类名
+    CrDom.prototype.setClass = function (clas) {
+        if (typeof clas === 'string') {
+            for (let i of this.dom) {
+                i.className = clas
+            }
+        }
+        return this
+    }
+
+    //TODO 添加类名
+    CrDom.prototype.addClass = function (clas) {
+        if (typeof clas === 'string') {
+            for (let i of this.dom) {
+                i.classList.add(clas)
+            }
+        }
+        return this
+    }
+```
