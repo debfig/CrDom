e623231e141efa60082ee2de3aad2008cf7d2510
灿烈 <3233834670@qq.com>
2022-11-02 21:41:10 +0800

    重写数据监听方法

--- CrDom.js
+++ CrDom.js
@@ -108,7 +108,7 @@ URL:https://github.com/debfig/CrDom
                 temp.push(i.innerText);
             });
             this.txt = copyArr(temp);
-        } else if (typeof txt == 'string') {
+        } else if (typeof txt == 'string' || typeof txt === 'number') {
             ergodic(this.dom, function (i) {
                 temp.push(i.innerText = txt);
             });
@@ -377,56 +377,75 @@ URL:https://github.com/debfig/CrDom
     //====================================================================
 
     //TODO 数据监听
-    $.DataBroker = function (object, value, fun, state = false) {
-        function ArrayBroker(arr, fun) {
-            let newPrototype = Object.create(Array.prototype);
-            let methods = ["push", "pop", "shift", "unshift", "reverse", "sort", "splice"];
-            methods.forEach(method => {
-                newPrototype[method] = function (...args) {
-                    //! 关键部分 我们使用延时定时器将函数调用由同步变为异步操作
-                    //! 这步是为了让对数组的的操作先执行，在执行函数的调用
-                    setTimeout(function () {
-                        fun(method);
-                    }, 0);
-                    return Array.prototype[method].call(this, ...args);
-                };
-            });
-            arr.__proto__ = newPrototype;
-            //判断数组中是否有对象和数组如果有进行get set和数组监听
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[i] instanceof Array) {
-                    ArrayBroker(arr[i], fun);
-                } else if ((arr[i] instanceof Object) && (typeof arr[i] == 'object')) {
-                    let temp = arr[i];
-                    arr[i] = new Object();
-                    $.DataBroker(arr[i], temp, fun);
-                }
-            }
-        };
-        for (let i in value) {
-            if (value[i] instanceof Array) {
-                //!         防止对原数组的更改
-                object[i] = [...value[i]];
-                ArrayBroker(object[i], fun);
-            } else if ((value[i] instanceof Object) && (typeof value[i] == 'object')) {
-                let obj = new Object();
-                object[i] = obj;
-                $.DataBroker(object[i], value[i], fun);
-            } else {
+    $.DataBroker = function (object, value, fun, state = true) {
+        //内部函数方便自调用
+        let DataMonitor = function (d_object, d_value, d_fun) {
+            //设置get和set 函数
+            function monitor(object, i, addobject) {
                 Object.defineProperty(object, i, {
                     enumerable: true,
                     get() {
-                        return value[i];
+                        return addobject;
                     },
                     set(val) {
-                        value[i] = val;
-                        fun(i);
+                        addobject = val;
+                        d_fun(i);
                     }
                 });
             };
+            //数组API劫持 函数
+            function ArrayBroker(arr, funs) {
+                let newPrototype = Object.create(Array.prototype);
+                let methods = ["push", "pop", "shift", "unshift", "reverse", "sort", "splice"];
+                methods.forEach(method => {
+                    newPrototype[method] = function (...args) {
+                        //! 关键部分 我们使用延时定时器将函数调用由同步变为异步操作
+                        //! 这步是为了让对数组的的操作先执行，在执行函数的调用
+                        setTimeout(function () {
+                            funs(method);
+                        }, 0);
+                        return Array.prototype[method].call(this, ...args);
+                    };
+                });
+                arr.__proto__ = newPrototype;
+                //判断数组中是否有对象和数组如果有进行get set和数组监听
+                for (let i = 0; i < arr.length; i++) {
+                    if (arr[i] instanceof Array) {
+                        ArrayBroker(arr[i], funs);
+                    } else if (arr[i] instanceof HTMLElement ? false : (arr[i] instanceof Object) && (typeof arr[i] == 'object')) {
+                        let temp = arr[i];
+                        arr[i] = new Object();
+                        arr[i].__proto__ = temp.__proto__;
+                        DataMonitor(arr[i], temp, funs);
+                    }
+                }
+            };
+
+            //判断数据类型并做对应操作 分为 数组 Dom对象 对象 其他
+            for (let i in d_value) {
+                //for in 会遍历对象原型上自定义的方法
+                //使用 hasOwnProperty 来判断是否是对象上自生的
+                if (d_value.hasOwnProperty(i)) {
+                    if (d_value[i] instanceof Array) {
+                        //!数组解构赋值防止对原数组的更改
+                        monitor(d_object, i, [...d_value[i]]);
+                        ArrayBroker(d_object[i], d_fun);
+                    } else if (d_value[i] instanceof HTMLElement) {
+                        monitor(d_object, i, d_value[i]);
+                    } else if ((d_value[i] instanceof Object) && (typeof d_value[i] == 'object')) {
+                        monitor(d_object, i, new Object());
+                        d_object[i].__proto__ = d_value[i].__proto__;
+                        DataMonitor(d_object[i], d_value[i], d_fun);
+                    } else {
+                        monitor(d_object, i, d_value[i]);
+                    }
+                }
+            }
         };
-        if (state) { fun(); };
-    };
+        //调用一次
+        DataMonitor(object, value, fun);
+        if (state) { fun() };
+    }
 
     //TODO 渲染表格
     $.CreateTable = function (dom, data, sort) {
